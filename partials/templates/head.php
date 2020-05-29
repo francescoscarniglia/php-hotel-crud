70151398d6a40127022a0b953abbd46641fbe5b1
Francesco Scarniglia <francescoscarniglia@gmail.com>
2020-05-29 07:48:50 +0200

    continue

---
+++ partials/templates/head.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta http-equiv="X-UA-Compatible" content="ie=edge">
+  <link rel="stylesheet" href="./dist/css/app.css">
+  <title>Php Hotel Crud | F.sco Scarniglia</title>
+</head>
+<body>
---
+++ partials/templates/home_content.php
@@ -0,0 +1,48 @@
+<main class="container">
+  <div class="row">
+    <div class="col-sm-12">
+      <header class="my-4">
+        <h1 class="text-center text-primary">Hotel Rooms</h1>
+      </header>
+      <table class="table">
+        <thead>
+          <tr>
+            <th>ID</th>
+            <th>N° room</th>
+            <th>Floor</th>
+            <th></th>
+            <th></th>
+          </tr>
+        </thead>
+        <tbody>
+            <?php //loop on db rooms
+            if(!empty($rooms)){
+              foreach ($rooms as $room) { ?>
+              <tr>
+                <td><?php echo $room['id'] ?></td>
+                <td><?php echo $room['room_number'] ?></td>
+                <td><?php echo $room['floor'] ?></td>
+                <td>
+                  <a class="text-success" href="#">
+                    View
+                  </a>
+                </td>
+                <td>
+                  <a class="text-primary" href="#">
+                    Update
+                  </a>
+                </td>
+                <td>
+                  <a class="text-danger" href="#">
+                    Delete
+                  </a>
+                </td>
+              </tr>
+              <?php }
+            }
+             ?>
+        </tbody>
+      </table>
+    </div>
+  </div>
+</main>
